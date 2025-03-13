2a16da44cba4129d1d4e49a8cd9a7d09207974f7
Jialin Yi <jialinyi@sas.upenn.edu>
2025-03-13 16:39:44 +0000

    FEAT: Implement Ornstein-Uhlenbeck process function and corresponding unit tests

---
+++ empyrical/mean_reverting.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+from statsmodels.regression.linear_model import OLS
+
+
+def ornstein_uhlenbeck(X: pd.Series, delta_t: float = 1/252):
+    X_prev = X.shift(1).dropna().values.reshape(-1,1)
+    delta_X = X.diff().dropna().values
+    model = OLS(delta_X, X_prev)
+    results = model.fit()
+    b = results.params[0]
+    residuals = results.resid
+    k = -np.log(b) / delta_t
+    m = X.mean()
+    sigma = np.sqrt(np.var(residuals) / (2*k))
+    return k, m, sigma
