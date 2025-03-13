62b46924da866ea2e7ddfe95d73d4ec2d339c84d
Jialin Yi <jialinyi@sas.upenn.edu>
2025-03-13 16:45:25 +0000

    DOC: Add docstring to ornstein_uhlenbeck function detailing parameters and return values

--- empyrical/mean_reverting.py
+++ empyrical/mean_reverting.py
@@ -4,6 +4,26 @@ from statsmodels.regression.linear_model import OLS
 
 
 def ornstein_uhlenbeck(X: pd.Series, delta_t: float = 1/252):
+    """ Ornstein-Uhlenbeck process
+
+    dX = k*(m-X)*dt + sigma*dW
+
+    Parameters
+    ----------
+    X : pd.Series
+        Time series of the process
+    delta_t : float, optional
+        the time bar measured in years , by default 1/252
+
+    Returns
+    -------
+    k : float
+        mean reversion speed
+    m : float
+        mean level
+    sigma : float
+        volatility of the process
+    """
     X_prev = X.shift(1).dropna().values.reshape(-1,1)
     delta_X = X.diff().dropna().values
     model = OLS(delta_X, X_prev)
